fee93ae712f9dc02c738ab3415ba691212c05f1d
saif qamar <adlyaqeen2@gmail.com>
2023-05-09 21:59:19 +0500

    made hero, work, workdet and contact sections responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,8 @@ const Hero = () =>{
         <div className="hero">
             <div className="container main-head-flex">
                 <div className="main-head" id="">
+                    <div className="back-blur"></div>
+                    <div className="back-blur-red"></div>
                     <div className="line-social">
                         <div className="line"></div>
                         <div class="social-media">
@@ -16,7 +18,7 @@ const Hero = () =>{
                         </div>
                     </div>
                     <div className="heading">
-                        <h1>Hi I am Saif,</h1>
+                        <h1>Hello, <br /> I'm Saif</h1>
                         <p>a UIUX Designer and Front-end Developer who is passionate about creating Digital epxeriences Through problem solving.</p>
                     </div>
 
