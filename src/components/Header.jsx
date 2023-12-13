03ab147ea768bca774d686b3a763e8941f747dc2
saif qamar <adlyaqeen2@gmail.com>
2023-12-13 19:56:30 +0500

    small change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,26 +14,26 @@ const Header = () => {
 
   return (
     <div className="bak"  data-aos="fade-down" data-aos-duration='1000'>
-    <div className='header' id='home'>
-      <div className="container nav-header">
-        <HashLink exact to={'/SaifQamarPortfolio#home'} className={'link'}>
-          <div className="logo" style={{display:'flex', width:'50%', padding:'20px', alignItems:'baseline', columnGap:'5px'}}>
-            {/* https://drive.google.com/file/d//view?usp=sharing */}
-              <h3 style={{color:'white'}}><img style={{height:'30px',}} src='https://drive.google.com/uc?export=view&id=1C7iF95dduOltPMd2q0qweus11eEmLmTg' /></h3><span style={{color:'#7979ea', fontWeight:'bold', fontSize:'10px'}}>Beta</span>
-          </div>
-        </HashLink>
-        <button class="nav-toggle" onClick={showlist}>
-            <i class="fas fa-bars"></i>
-        </button>
+      <div className='header' id='home'>
+        <div className="container nav-header">
+          <HashLink exact to={'/SaifQamarPortfolio#home'} className={'link'}>
+            <div className="logo" style={{display:'flex', width:'50%', padding:'20px', alignItems:'baseline', columnGap:'5px'}}>
+              {/* https://drive.google.com/file/d//view?usp=sharing */}
+                <h3 style={{color:'white'}}><img style={{height:'30px',}} src='https://drive.google.com/uc?export=view&id=1C7iF95dduOltPMd2q0qweus11eEmLmTg' /></h3><span style={{color:'#7979ea', fontWeight:'bold', fontSize:'10px'}}>Beta</span>
+            </div>
+          </HashLink>
+          <button class="nav-toggle" onClick={showlist}>
+              <i class="fas fa-bars"></i>
+          </button>
+        </div>
+        <ul className={isSmallScreen ? "links show-links" : "links"}>
+        {/* <HashLink exact to={'/SaifQamarPortfolio#home'} className={'link'}><li>Home</li></HashLink> */}
+        <HashLink exact to={'/SaifQamarPortfolio#work'}  className={'link'}><li>Projects</li></HashLink>
+        {/* <HashLink exact to={'/SaifQamarPortfolio#work'}  className={'link'}><li>Blogs</li></HashLink> */}
+        <HashLink exact to={'/SaifQamarPortfolio#about'} className={'link'}><li>About</li></HashLink>
+        <HashLink exact to={'/SaifQamarPortfolio/contact'} className={'link'}><li>Contact</li></HashLink>        
+        </ul>
       </div>
-      <ul className={isSmallScreen ? "links show-links" : "links"}>
-      {/* <HashLink exact to={'/SaifQamarPortfolio#home'} className={'link'}><li>Home</li></HashLink> */}
-      <HashLink exact to={'/SaifQamarPortfolio#work'}  className={'link'}><li>Projects</li></HashLink>
-      {/* <HashLink exact to={'/SaifQamarPortfolio#work'}  className={'link'}><li>Blogs</li></HashLink> */}
-      <HashLink exact to={'/SaifQamarPortfolio#about'} className={'link'}><li>About</li></HashLink>
-      <HashLink exact to={'/SaifQamarPortfolio/contact'} className={'link'}><li>Contact</li></HashLink>        
-      </ul>
-    </div>
     </div>
   )
 }
